2f694c8ebfbf99c2cf0c97154e5ae1f7f423163a
Caleb <caleblpena@gmail.com>
2025-07-23 14:58:46 -0600

    add user menu

--- src/routes/family/components/nav-bar.tsx
+++ src/routes/family/components/nav-bar.tsx
@@ -1,18 +1,43 @@
-import { House, Mail, HeartHandshake, ListChecks } from 'lucide-react'
+import {
+  House,
+  Mail,
+  HeartHandshake,
+  ListChecks,
+  MessageCircleQuestionMark,
+  LogOut,
+  UserRound,
+} from 'lucide-react'
 import { NavBar } from '@/components/nav-bar'
 import { useFamilyContext } from '../wrapper'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-} from '@/components/ui/select'
 import { useNavigate } from '@tanstack/react-router'
+import {
+  SignOutButton,
+  useClerk,
+  UserButton,
+  useUser,
+} from '@clerk/clerk-react'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export function FamilyNavBar() {
   const { selectedChildInfo, navBar, children } = useFamilyContext()
   const navigate = useNavigate()
+  const clerk = useClerk()
+  const { user, isLoaded, isSignedIn } = useUser()
+
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
 
   if (navBar.hidden) {
     return null
@@ -27,23 +52,57 @@ export function FamilyNavBar() {
       <div className="flex justify-between bg-primary text-primary-foreground p-5">
         <img src="/logo.png" className="max-h-15" alt="logo" />
         <div className="flex items-center">
-          <Select
-            value={String(selectedChildInfo.id)}
-            onValueChange={changeChild}
-          >
-            <SelectTrigger>
-              {selectedChildInfo.firstName} {selectedChildInfo.lastName}
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src={user.imageUrl} />
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Select Child</DropdownMenuLabel>
+              <DropdownMenuRadioGroup
+                value={String(selectedChildInfo.id)}
+                onValueChange={changeChild}
+              >
                 {children.map((child) => (
-                  <SelectItem value={String(child.id)} key={child.id}>
+                  <DropdownMenuRadioItem
+                    value={String(child.id)}
+                    key={child.id}
+                  >
                     {child.firstName} {child.lastName}
-                  </SelectItem>
+                  </DropdownMenuRadioItem>
                 ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+              </DropdownMenuRadioGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <a href="https://google.com/" target="_blank" rel="noopener">
+                    {/*TODO: add help link*/}
+                    <MessageCircleQuestionMark />
+                    Help/Support
+                  </a>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <button
+                    onClick={() => {
+                      clerk.openUserProfile()
+                    }}
+                  >
+                    <UserRound />
+                    Your Profile
+                  </button>
+                </DropdownMenuItem>
+                <DropdownMenuItem variant="destructive" asChild>
+                  <SignOutButton>
+                    <span>
+                      <LogOut />
+                      Sign Out
+                    </span>
+                  </SignOutButton>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
       <div className="flex justify-center items-center p-5 bg-white">
