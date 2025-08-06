f4252d5106eeadc6db3d85485c09b0d0973bf0d9
tlillis <thomasglillis@gmail.com>
2025-08-06 13:51:59 -0600

    update size

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ export function Logo({ to }: { to: string }) {
     <Link to={to}>
       <img
         src={isMobile ? "/cap_circle_logo_white.png" : "/cap_full_logo_white.png"}
-        className="max-h-12"
+        className="max-h-8"
         alt="logo"
       />
     </Link>
