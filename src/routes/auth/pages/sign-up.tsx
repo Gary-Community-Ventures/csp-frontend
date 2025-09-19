0d6d44289d83515b1f85de8f556045c6b9b2a9eb
Thomas Lillis <thomasglillis@gmail.com>
2025-09-19 11:14:24 -0600

    [Feat] Lang toggle on sign in page (#117)
    * working
    
    * formatting
    
    * make dropdown
    
    * update to use unified component
    
    * revert

--- src/routes/auth/pages/sign-up.tsx
+++ src/routes/auth/pages/sign-up.tsx
@@ -1,9 +1,11 @@
 import { SignUp } from '@clerk/clerk-react'
 import { signInRoute } from '../routes'
+import { LanguageToggle } from '@/components/language-toggle'
 
 export function SignUpPage() {
   return (
-    <div>
+    <div className="relative">
+      <LanguageToggle />
       <SignUp signInUrl={signInRoute.to} />
     </div>
   )
